9a867e153bb1e83cfea1dbbc2be9c645c618f876
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-13 14:42:41 -0600

    TEST- mapquest service to retrieve coordinates for a given city and state. All testing passes.

--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -1,3 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 
+RSpec.describe MapQuestService do 
+  describe 'Coordinates' do 
+    it 'gets the lat/lng for a given city and state', :vcr do 
+      coordinates = MapQuestService.coordinates('nashville, tn')
+      
+      expect(coordinates).to be_a Hash 
+      expect(coordinates[:results][0][:locations][0][:latLng]).to be_a Hash
+      expect(coordinates[:results][0][:locations][0][:latLng]).to have_key :lat
+      expect(coordinates[:results][0][:locations][0][:latLng][:lat]).to be_a Float
+      expect(coordinates[:results][0][:locations][0][:latLng]).to have_key :lng 
+      expect(coordinates[:results][0][:locations][0][:latLng][:lat]).to be_a Float 
+    end
+  end
+end
