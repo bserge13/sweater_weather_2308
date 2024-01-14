ef5aabf69fdac709f1dc879933c7d519fc111ffe
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-14 16:39:19 -0600

    Completeion of happy path testing for munchies api service. All tests passing. Moving on to poro, service, and facade testing of munchie api.

--- app/poro/munchie.rb
+++ app/poro/munchie.rb
@@ -11,6 +11,5 @@ class Munchie
     @destination_city = destination
     @forecast = forecast
     @restaurant = munchie_data
-    require 'pry'; binding.pry
   end
 end
--- spec/requests/api/v1/get_munchies_spec.rb
+++ spec/requests/api/v1/get_munchies_spec.rb
@@ -13,7 +13,34 @@ RSpec.describe 'Munchie API' do
       results = JSON.parse(response.body, symbolize_names: true)
 
       expect(results).to be_a Hash 
-      require 'pry'; binding.pry
+      expect(results[:data]).to have_key :id
+      expect(results[:data][:id]).to eq nil 
+      expect(results[:data]).to have_key :type
+      expect(results[:data][:type]).to eq 'munchie' 
+      expect(results[:data]).to have_key :attributes 
+      expect(results[:data][:attributes]).to be_a Hash 
+
+      yelp = results[:data][:attributes]
+      
+      expect(yelp).to have_key :destination_city
+      expect(yelp[:destination_city]).to be_a String 
+      expect(yelp[:destination_city]).to eq("Evansville, IN")
+      expect(yelp).to have_key :forecast
+      expect(yelp[:forecast]).to be_a Hash 
+      expect(yelp).to have_key :restaurant
+      expect(yelp[:restaurant]).to be_an Array 
+
+      food = yelp[:restaurant][0]
+
+      expect(food).to be_a Hash
+      expect(food).to have_key :name
+      expect(food[:name]).to be_a String
+      expect(food).to have_key :address
+      expect(food[:address]).to be_a String
+      expect(food).to have_key :rating
+      expect(food[:rating]).to be_a Float
+      expect(food).to have_key :reviews
+      expect(food[:reviews]).to be_an Integer
     end
   end
 end
