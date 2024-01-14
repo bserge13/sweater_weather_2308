1d4d6fd3f80ca09b78aa9e899dd0b1f3e32c86bf
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-14 15:26:09 -0600

    Establish connection to yelp api. Connection good, credentials good, moving to debug facade for munchie object creations.

--- app/services/munchies_service.rb
+++ app/services/munchies_service.rb
@@ -1,5 +1,18 @@
 class MunchiesService 
   def self.find_food(destination, food)
-    
+    get_url("/v3/businesses/search?location=#{destination}&term=#{food}")
   end 
+
+  private
+
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn 
+    Faraday.new(url: 'https://api.yelp.com') do |f|
+      f.headers['Authorization'] = Rails.application.credentials.yelp[:YELP_API_KEY]
+    end
+  end
 end
