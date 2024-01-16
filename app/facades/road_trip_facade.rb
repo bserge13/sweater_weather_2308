dc19385c45ece0280df603aa473dda6157dcc1e6
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-16 12:17:15 -0600

    road trip facade logic with conditional to ensure arrival time includes date.

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -11,6 +11,17 @@ class RoadTripFacade
       arrival_time = Time.now + travel_time
       formatted_arrival = arrival_time.strftime('%Y-%m-%d %H:%M')
       formatted_travel = directions[:route][:formattedTime]
+
+      forecast = WeatherService.daily_forecast(dest_lat, dest_lng)
+      forecast[:forecast][:forecastday].map do |day|
+        next unless arrival_time.to_s.include?(day[:date])
+        @arrival_forecast = {
+          datetime: formatted_arrival,
+          temperature: day[:hour][arrival_time.hour][:temp_f],
+          condition: day[:hour][arrival_time.hour][:condition][:text]
+        }
+      end
+      RoadTrip.new(origin, destination, formatted_travel, @arrival_forecast)
     end
   end
 end
