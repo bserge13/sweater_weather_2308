42e9d88febf5c598d385af901a312ca202bbf0d1
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-15 20:44:17 -0600

    TEST- sad path testing for invalid password for post session request. All tests passing.

--- spec/requests/api/v0/post_session_spec.rb
+++ spec/requests/api/v0/post_session_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe 'Post request- sessions' do
       post '/api/v0/sessions', headers: header, params: payload, as: :json
 
       expect(response).to be_successful 
+      expect(response.status).to eq 200 
 
       session = JSON.parse(response.body, symbolize_names: true)
 
@@ -39,4 +40,30 @@ RSpec.describe 'Post request- sessions' do
       expect(session[:data][:attributes][:api_key]).to eq(@db_user.api_key)
     end
   end
+
+  describe 'sad path' do 
+    it 'cannot create a session with a bad password' do 
+      payload = {
+        email: 'winchester1@gmail.com',
+        password: 'password123',
+      }
+
+      header = {
+        'CONTENT_TYPE' => 'application/json',
+        'ACCEPT' => 'application/json'
+      }
+
+      post '/api/v0/sessions', headers: header, params: payload, as: :json
+
+      expect(response).to_not be_successful 
+      expect(response.status).to eq 400 
+
+      error = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(error).to be_a Hash
+      expect(error).to have_key :errors
+      expect(error[:errors]).to be_a String
+      expect(error[:errors]).to eq 'Invalid credentials'
+    end
+  end 
 end
