41f85687c55fb3f3afaa1be1af507778b4cc9f50
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-15 18:28:38 -0600

    TEST- happy path testing of a user post request and json return. All tests passing.

--- spec/requests/api/v0/post_user_spec.rb
+++ spec/requests/api/v0/post_user_spec.rb
@@ -23,7 +23,14 @@ RSpec.describe 'Users POST request' do
 
       expect(user).to be_a Hash 
       expect(user).to have_key :data 
-
+      expect(user[:data]).to have_key :id
+      expect(user[:data]).to have_key :type
+      expect(user[:data][:type]).to eq 'user'
+      expect(user[:data]).to have_key :attributes
+      expect(user[:data][:attributes]).to have_key :email
+      expect(user[:data][:attributes][:email]).to be_a String
+      expect(user[:data][:attributes]).to have_key :api_key
+      expect(user[:data][:attributes][:api_key]).to be_a String 
     end
   end
 end
